ca0ad334c1bf3c5dbe4ab11db513ed9691fa7781
Aleksandr A. Timofeev <taratatuy@yandex.ru>
2025-04-06 22:38:28 +0300

    improvements of tmux-navigate script

--- stow/.scripts/tmux-navigate.sh
+++ stow/.scripts/tmux-navigate.sh
@@ -1,34 +1,42 @@
 #!/bin/zsh
 
 if [ -z "$2" ]; then
-  echo "At least 2 arguments expected, but got $#" 
-  echo "Usage: ./tmux-navigate <session-name> <session-path> [root-session-command] [[path command] ...]" 
+  echo "Error: At least 2 arguments expected, but got $#" 
+  echo "Usage: ./tmux-navigate session-name tab-path [tab-command] [[tab-path [tab-command]] ...]" 
   exit 1
 fi
 
-SESSION_NAME=$1
-shift
-SESSION_PATH=$1
-shift
-ROOT_SESSION_COMMAND=$1
-shift
+session_name=$1; shift
+first_path=$1; shift
+first_command=$1; shift 2> /dev/null
 
-tmux has-session -t $SESSION_NAME &> /dev/null
-if [ $? != 0 ]
-then
-  tmux new -s $SESSION_NAME -d -c "$SESSION_PATH" $ROOT_SESSION_COMMAND
-
-  if [ $# -gt 0 ]; then
-    for ((i=0; i < $#; i+=2)); do
-      WINDOW_PATH="${@:$(($i+1)):1}"
-      COMMAND_NAME="${@:$(($i+2)):1}"
-
-      tmux new-window -t $SESSION_NAME -c "$WINDOW_PATH" $COMMAND_NAME
-    done
-  fi
-
-  tmux switchc -t $SESSION_NAME:1
-else
-  tmux switchc -t $SESSION_NAME
+tmux has-session -t $session_name &> /dev/null
+if [ $? -eq 0 ]; then
+  tmux switchc -t $session_name
+  exit 0
 fi
+
+if [ ! -d $first_path ]; then
+  echo "Error: Directory does not exist: $first_path"
+  exit 2
+fi
+
+tmux new -s $session_name -d -c "$first_path" $first_command
+
+if [ $# -gt 0 ]; then
+  for ((i=0; i < $#; i+=2)); do
+    window_path="${@:$(($i+1)):1}"
+    window_command="${@:$(($i+2)):1}"
+
+    if [ ! -d $window_path ]; then
+      echo "Warn: Tab skipped. Directory does not exist: $first_path"
+      sleep 2
+      break
+    fi
+
+    tmux new-window -t $session_name -c "$window_path" $window_command
+  done
+fi
+
+tmux switchc -t $session_name:1
 
