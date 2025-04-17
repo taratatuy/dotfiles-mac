7305d9791a1ca7b1113e840a60fe9c437117a5d7
Aleksandr A. Timofeev <taratatuy@yandex.ru>
2025-04-17 19:48:11 +0300

    adjust todo comments highlight

--- stow/.config/nvim/lua/plugins/todo-comments.lua
+++ stow/.config/nvim/lua/plugins/todo-comments.lua
@@ -2,5 +2,14 @@ return {
   "folke/todo-comments.nvim",
   event = "VimEnter",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = { signs = false },
+  opts = {
+    signs = false,
+    keywords = {
+      TODO = { icon = " ", color = "info", alt = { "TODO ALTI0821" } },
+    },
+    highlight = {
+      pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+    },
+    pattern = [[\b(KEYWORDS)\b]],
+  },
 }
