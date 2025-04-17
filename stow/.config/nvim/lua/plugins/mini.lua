237247981cc48cc7b32cffd90180d94beaec43dc
Aleksandr A. Timofeev <taratatuy@yandex.ru>
2025-04-17 19:48:12 +0300

    edit status line look

--- stow/.config/nvim/lua/plugins/mini.lua
+++ stow/.config/nvim/lua/plugins/mini.lua
@@ -20,8 +20,37 @@ return { -- Collection of various small independent plugins/modules
     -- default behavior. For example, here we set the section for
     -- cursor location to LINE:COLUMN
     ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return "%2l:%-2v"
+    statusline.section_location = function(args)
+      -- Use virtual column number to allow update when past last column
+      if statusline.is_truncated(args.trunc_width) then
+        return "%l %2v"
+      end
+
+      -- Use `virtcol()` to correctly handle multi-byte characters
+      return '%l:%L %2v:%-2{virtcol("$") - 1}'
+    end
+
+    vim.api.nvim_set_hl(0, "MiniStatuslineModeNormal", {})
+    vim.api.nvim_set_hl(0, "MiniStatuslineModeInsert", { link = "DiffText" })
+
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_fileinfo = function(args)
+      local filetype = vim.bo.filetype
+      if (filetype == "") or vim.bo.buftype ~= "" then
+        return ""
+      end
+      local file_name, file_ext = vim.fn.expand("%:t"), vim.fn.expand("%:e")
+
+      local devicons = require("nvim-web-devicons")
+      local icon = devicons.get_icon(file_name, file_ext, { default = true })
+      if icon ~= "" then
+        filetype = string.format("%s %s", icon, filetype)
+      end
+
+      if statusline.is_truncated(args.trunc_width) then
+        return filetype
+      end
+      return filetype
     end
 
     -- ... and there is more!
