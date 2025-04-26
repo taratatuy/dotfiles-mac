61fc97fb813fcd17e66cc7acc31fc1c613fa92b3
Aleksandr A. Timofeev <taratatuy@yandex.ru>
2025-04-26 22:55:46 +0300

    update convert-mov-to-mp4.sh
    update convert-mov-to-mp4.sh

--- stow/.scripts/convert-mov-to-mp4.sh
+++ stow/.scripts/convert-mov-to-mp4.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-if [ -z $2 ]; then
-  echo "Usage: ./convert-mov-to-mp4.sh <input_name> <output_name>"
+set -euo pipefail
+
+input_file_name="${1:-}"
+output_file_name="${2:-}"
+
+if [ -z "${input_file_name}" ]; then
+  echo "Usage: ./convert-mov-to-mp4.sh <input_name> [<output_name>]"
   exit 1
 fi
 
-ffmpeg -i "$1" -vcodec libx264 -acodec aac "$2"
+if [ ! -f "${input_file_name}" ]; then
+  echo "Error: No file found ${input_file_name}"
+  exit 1
+fi
+
+if [ -z "${output_file_name}" ]; then
+  output_file_name="${input_file_name%.mov}.mp4"
+  output_file_name="${output_file_name// /_}"
+fi
+
+ffmpeg -i "${input_file_name}" -vcodec libx264 -acodec aac "${output_file_name}"
