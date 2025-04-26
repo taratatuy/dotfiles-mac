40cbafbc4569c224da1b8f414a4309e757caf04c
Aleksandr A. Timofeev <taratatuy@yandex.ru>
2025-04-26 23:58:23 +0300

    update curl-helper.sh

--- stow/.scripts/curl-helper.sh
+++ stow/.scripts/curl-helper.sh
@@ -1,74 +1,86 @@
 #!/bin/bash
 set -euo pipefail
 
-# The script expects execute_curl to be declared first.
+# The script expects execute_curl() and print_usage() to be declared first.
 # Example:
 #
 # execute_curl() {
-#   local env="${1}" server="${2}" token="${3}"
+#   local env="${1}" server="${2}" token="${3}" other_arg1="${4}"
 #
 #   url="https://${env}.${server}"
 #
 #   curl --location "${url}" \
 #     -H "Authorization: ${token}"
 # }
-#
+# print_usage(){
+#   echo "Usage: ${0} [-n] mdc-01/env-1 [other_arg1 other_arg2 ...]"
+# }
 # source ~/.scripts/curl-helper.sh $@
 
 tmp_folder="/tmp/curl"
 
-if [ "${1:-}" == "-n" ]; then
-  set_new_token="true"
-  shift 1
-fi
+for arg in "${@:-}"; do
+  if [ "$arg" != "-n" ]; then
+    filtered_args+=("$arg")
+  else
+    set_new_token="true"
+  fi
+done
 
-if [ -z "${1:-}" ]; then
+set -- "${filtered_args[@]:-}"
+
+if [ "${#}" == 0 ]; then
   echo "Error: Error while parsing the env route."
-  echo "Usage: ./curl-helper.sh [-n] mdc-01/env-1"
+  print_usage
   exit  1
 fi
 
 IFS='/' read -ra env_routes <<< "${1}"
+shift
 
 if [ "${#env_routes[@]}" -lt 2 ]; then
   echo "Error: Error while parsing the env route."
-  echo "Usage: ./curl-helper.sh [-n] mdc-01/env-1"
+  print_usage
   exit  1
 fi
 
 server="${env_routes[0]}"
 env="${env_routes[1]}"
+env_token_file="${tmp_folder}/${server}_${env}_token"
 
 if [ ! -d "${tmp_folder}" ]; then
   mkdir "${tmp_folder}"
 fi
 
-if [ "${set_new_token:-}" == "true" ] && [ -f "${tmp_folder}/curl_token" ]; then
-  mv "${tmp_folder}/curl_token" "${tmp_folder}/curl_token.bkp"
+if [ "${set_new_token:-}" == "true" ] && [ -f "${env_token_file}" ]; then
+  mv "${env_token_file}" "${env_token_file}.bkp"
 fi
 
-if [ -f "${tmp_folder}/curl_token" ]; then
-  token=$(<"${tmp_folder}/curl_token")
+if [ -f "${env_token_file}" ]; then
+  token=$(<"${env_token_file}")
 else
-  ${EDITOR} "${tmp_folder}/curl_token"
+  ${EDITOR} "${env_token_file}"
 
-  if [ ! -f "${tmp_folder}/curl_token" ] && [ -f "${tmp_folder}/curl_token.bkp" ]; then
-    mv "${tmp_folder}/curl_token.bkp" "${tmp_folder}/curl_token"
-  elif [ -f "${tmp_folder}/curl_token.bkp" ]; then
-    rm "${tmp_folder}/curl_token.bkp"
+  if [ ! -f "${env_token_file}" ] && [ -f "${env_token_file}.bkp" ]; then
+    mv "${env_token_file}.bkp" "${env_token_file}"
+  elif [ -f "${env_token_file}.bkp" ]; then
+    rm "${env_token_file}.bkp"
   fi
 
-  token=$(<${tmp_folder}/curl_token)
+  token=$(<"${env_token_file}")
 fi
 
 current_time="$(perl -MTime::HiRes -E 'say int(Time::HiRes::time() * 1000)')"
-file_name="${tmp_folder}/${0}-${current_time}.response"
+response_file_name=${0#./}
+response_file_name=${response_file_name%.curl.sh}
+response_file_name="${tmp_folder}/${response_file_name}.${server}_${env}.${@:-}.${current_time}.response"
+response_file_name="${response_file_name// /_}"
 
-execute_curl "${env}" "${server}" "${token}" > "${file_name}"
+execute_curl "${env}" "${server}" "${token}" "${@:-}" > "${response_file_name}"
 
 echo ""
 echo "Url: ${url}"
 echo ""
-echo "Saved to ${file_name}"
-${EDITOR} "${file_name}"
+echo "Saved to ${response_file_name}"
+${EDITOR} "${response_file_name}"
 
